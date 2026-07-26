8ab9033e91f34b136cca7ed2f38de5570f4a2d94
0xyusufz <gurucoc471@gmail.com>
2026-07-26 22:01:11 +0530

    added all logic

--- project.py
+++ project.py
@@ -20,6 +20,49 @@ def check_winner(b):
         return "DRAW"
     return None
 
-
+current = 1
+print("Welcome to Tic-Tac-Toe")
 print_board(board)
+
+while True:
+    if current == 1:
+        player = "X"
+    else:
+        player = "O"
+    print(f"current chance : {player}")
+    try:
+        row = int(input(f"{player}: enter the row b/w 0 to 2: "))
+        if row >2 or row < 0:
+            raise Exception("please enter b/w 0 to 2: ")
+        col = int(input(f"{player}: enter the column b/w 0 to 2: "))
+        if col >2 or row < 0:
+            raise Exception("please enter b/w 0 to 2: ")
+    except ValueError:
+        print("please enter th number only \n")
+        continue
+    except Exception as e:
+        print(f"something went wrong \n{e} \n")
+        continue
+    
+    if board[row,col] != 0:
+        print("cell already taken")
+        print_board(board)
+        continue
+    board[row,col] = current
+    print_board(board)
+    
+    result = check_winner(board)
+    if result is not None:
+        if result == "DRAW":
+            print("Match Draw")
+        else:
+            print(f"{result}: wins")
+        break
+    if current == 1:
+        current = -1
+        print(f"{current} from if")
+    else:
+        current = 1
+
+
 
