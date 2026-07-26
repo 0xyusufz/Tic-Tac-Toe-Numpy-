6a87e80c51107c9dad70fa963033bd2fae3c8f04
0xyusufz <gurucoc471@gmail.com>
2026-07-26 21:23:36 +0530

    added check_winner fnc

--- project.py
+++ project.py
@@ -11,5 +11,15 @@ def print_board(b):
             print("---+---+---")
     print()
 
+def check_winner(b):
+    if 3 in np.sum(b,axis= 1) or 3 in np.sum(b,axis = 0) or 3 == np.trace(b) or 3 == np.trace(np.fliplr(b)):
+        return "X"
+    if -3 in np.sum(b,axis= 1) or -3 in np.sum(b,axis = 0) or -3 == np.trace(b) or -3 == np.trace(np.fliplr(b)):
+        return "O"
+    if not 0 in b:
+        return "DRAW"
+    return None
+
+
 print_board(board)
 
